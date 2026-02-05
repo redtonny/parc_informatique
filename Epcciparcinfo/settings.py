ba319338857d8ed4bfb09b6ac7f05327cad2e544
Redtonny <emmanueldago57@gmail.com>
2026-02-05 11:08:37 +0000

    creations des models et des migrations

--- Epcciparcinfo/settings.py
+++ Epcciparcinfo/settings.py
@@ -132,8 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR/ 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+#STATICFILES_DIRS = [BASE_DIR/ 'static']
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL= 'media'
-MEDIA_ROOT = BASE_DIR / 'media'
+#MEDIA_URL= 'media'
+#MEDIA_ROOT = BASE_DIR / 'media'
