f8a567021f63dce8095ee1322051ebd32fd56385
Redtonny <emmanueldago57@gmail.com>
2026-02-09 16:32:12 +0000

    creation des urls pur chaque application et affichage de la vue dashboard

--- ticket/views.py
+++ ticket/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .models import Ticket
+from interventions.models import Intervention
+from django.db.models import Count
 
-# Create your views here.
+
+def dashboard(request):
+    total_tickets =  Ticket.objects.count()
+    
+    tickets_ouverts= Ticket.objects.filter(statut='ouvert').count()
+    tickets_fermes= Ticket.objects.filter(statut='fermer').count()
+    
+    interventions_terminees= Intervention.objects.filter(est_termine=True).count()
+    
+    techniciens= Intervention.objects.values('technicien__username').annotate(
+        total=Count('id')
+    ).order_by('-total')[:5]
+    
+    context={
+        'total_tickets':total_tickets,
+        'tickets_ouverts':tickets_ouverts,
+        'tickets_fermes':tickets_fermes,
+        'interventions_terminees':interventions_terminees,
+        'techniciens':techniciens,
+    }
+    
+    return render(request, 'dashboard.html', context)
